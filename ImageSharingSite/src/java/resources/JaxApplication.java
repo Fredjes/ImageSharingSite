06bdb328497a2165bd073958ca1184b72d91926f
Fredjes <frederik.de.smedt@hotmail.com>
2015-03-30 09:54:53 +0200

    JAX-RS Application added

---
+++ ImageSharingSite/src/java/resources/JaxApplication.java
@@ -0,0 +1,12 @@
+package resources;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ * @author Frederik
+ */
+@ApplicationPath("api")
+public class JaxApplication extends Application {
+}
